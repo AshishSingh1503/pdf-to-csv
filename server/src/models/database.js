7c8ca3e67bb3581c99be311e284cd7f36ab40495
RebelAKL <rebel7soul@duck.com>
2025-10-23 21:39:08 +0530

    final

--- server/src/models/database.js
+++ server/src/models/database.js
@@ -9,7 +9,9 @@ const pool = new Pool({
   database: process.env.DB_NAME || 'pdf2csv_db',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
-  ssl: false,
+  ssl: process.env.DB_SSL === 'true' ? {
+    rejectUnauthorized: false
+  } : false,
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
   connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
